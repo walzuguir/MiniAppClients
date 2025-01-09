bc53dcdcf6af66568d3be087ce4fee43cd3ff099
Wanderson Lucio Dantas <Wlucio@provertec.com.br>
2025-01-09 14:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDotNet1/ClienteRepositorio.cs
@@ -0,0 +1,128 @@
+﻿using Cadastro; // Importa o namespace Cadastro para a classe ClienteRepositorio
+
+namespace ClienteRepo;
+
+public class ClienteRepositorio // Classe criada para armazenar os clientes
+{
+
+    public List<Cliente> clientes = new List<Cliente>(); // Instanciando uma nova lista de clientes
+
+    public void ExcluirCliente() // Método para exluir um cliente da DB
+    {
+        Console.Clear();
+        Console.Write("Informe o código do cliente a ser excluido: ");
+        var codigo = Console.ReadLine();
+
+        var cliente = clientes.FirstOrDefault(p => p.Id == int.Parse(codigo)); // Atráves do método FirstOrDefault que retorna o primeiro elemento da sequência que satisfaça uma condição específica ou um valor padrão se nenhum elemento for encontrado.
+
+        if (cliente == null)
+        {
+            Console.WriteLine("Cliente não encontrado na DB! [Enter]");
+            Console.ReadKey();
+            return;
+        }
+
+        ImprimirCliente(cliente);
+
+        clientes.Remove(cliente); // Remove o cliente da lista de clientes
+
+        Console.WriteLine("Cliente removido com sucesso! [Enter]"); // Mensagem para informar que o cliente foi removido
+
+        Console.ReadKey();
+    }
+
+    public void EditarCliente() // Método para editar as informações do cliente
+    {
+        Console.Clear();
+        Console.Write("Informe o código do cliente: ");
+        var codigo = Console.ReadLine();
+
+        var cliente = clientes.FirstOrDefault(p => p.Id == int.Parse(codigo)); // Busca o cliente na lista de clientes por meio do ID informado pelo usuário final.
+
+        if (cliente == null) 
+        {
+            Console.WriteLine("Cliente não encontrado na DB! [Enter]");
+            Console.ReadKey();
+            return;
+
+        }
+
+        ImprimirCliente(cliente); // Método que imprime os dados do cliente
+
+        Console.Write("Nome do usuário: ");
+        var nome = Console.ReadLine();
+        Console.Write(Environment.NewLine);
+
+        Console.Write("Data de nascimento: ");
+        var dataDeNascimento = DateOnly.Parse(Console.ReadLine());
+        Console.Write(Environment.NewLine);
+
+        Console.Write("Desconto: ");
+        var desconto = decimal.Parse(Console.ReadLine());
+        Console.Write(Environment.NewLine);
+
+        cliente.Nome = nome;
+        cliente.DataNascimento = dataDeNascimento;
+        cliente.Desconto = desconto;
+        cliente.CadastradoEm = DateTime.Now;
+
+        Console.WriteLine("Cliente cadastrado com sucesso! [Enter]");
+        ImprimirCliente(cliente);
+        Console.ReadKey();
+    
+    }
+
+    
+    public void CadastrarCliente() // Método para efetivar o cadastro das informações do cliente...
+    {
+        Console.Clear();
+
+        Console.Write("Nome do cliente: ");
+        
+        var nome = Console.ReadLine();
+        Console.Write(Environment.NewLine);
+
+        Console.Write("Data de nascimento: ");
+        var dataDeNascimento = DateOnly.Parse(Console.ReadLine());
+        Console.Write(Environment.NewLine);
+
+        Console.Write("Desconto: ");
+        var desconto = decimal.Parse(Console.ReadLine());
+        Console.Write(Environment.NewLine);
+
+        var cliente = new Cliente(); // Instancia um novo cliente
+        cliente.Id = clientes.Count + 1; // Atribui um novo ID ao cliente que é relativo ao tamanho da lista +1
+        cliente.Nome = nome;
+        cliente.DataNascimento = dataDeNascimento;
+        cliente.Desconto = desconto;
+        cliente.CadastradoEm = DateTime.Now; // Atribui a data de cadastro do cliente na data atual
+
+        clientes.Add(cliente); // Adiciona o novo cliente a lista de clientes
+
+        Console.WriteLine("Cliente cadastrado com sucesso! [Enter]");
+        ImprimirCliente(cliente); // Método que imprime os dados do cliente no console
+        Console.ReadKey(); // Volta ao menu principal
+
+    }
+
+    public void ImprimirCliente(Cliente cliente) // Método que imprime os dados do cliente
+    {
+        Console.WriteLine("ID.........: " + cliente.Id);
+        Console.WriteLine("Nome.........: " + cliente.Nome);
+        Console.WriteLine("Desconto.........: " + cliente.Desconto.ToString("0.00")); // usa o método ToString para formatar o valor em 2 casas decimais.
+        Console.WriteLine("Data de nascimento.........: " + cliente.DataNascimento);
+        Console.WriteLine("Data cadastro..........: " + cliente.CadastradoEm); // Imprime a data de cadastro do cliente.
+        Console.WriteLine("----------------------------------------------");
+    }
+
+    public void ExibirClientes() //Método para exibir a lista de clientes.
+    {
+        Console.Clear();
+        foreach (var cliente in clientes) // Percorre a lista de clientes por completa
+        {
+            ImprimirCliente(cliente); //Método que imprime a lista de clientes
+        }
+        Console.ReadKey();
+    }
+
+}
---
+++ ExercicioDotNet1/Program.cs
@@ -0,0 +1,88 @@
+﻿using ClienteRepo; // Importa o namespace ClienteRepo para a classe Program para instanciar a classe ClienteRepositorio
+
+namespace AppClientes; // Namespace da aplicação
+
+class Program // Classe principal do programa que gerenci interação do usuário com o sistema de cadastro
+{
+
+    static ClienteRepositorio _clienteRepositorio = new ClienteRepositorio(); // Instanciando a classe ClienteRepositorio
+
+    static void Main(string[] args) // Método principal da class Program
+    {
+        while (true) // Mantem a aplicação em execução contínua...
+        {
+            Menu(); //Exibe o menu principal apartir do método Menu()
+
+            Console.ReadKey();
+        }
+    }
+
+    static void Menu() // Menu principal
+    {
+        Console.Clear();
+
+        Console.WriteLine("Cadastro de clientes");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("1 - Cadastrar Cliente");
+        Console.WriteLine("2 - Exibir Clientes");
+        Console.WriteLine("3 - Editar Cliente");
+        Console.WriteLine("4 - Excluir Cliente");
+        Console.WriteLine("5 - Sair");
+        Console.WriteLine("--------------------");
+
+        EscolherOpcao(); // Chama o método EscolherOpcao para o usuário escolher uma opção através do menu principal.
+    }
+
+    static void EscolherOpcao() // Método para ler o input do usuário e chamar o método correspondente
+    {
+        Console.WriteLine("Escolha uma opção válida: ");
+        try
+        {
+        
+        var opcao = Console.ReadLine(); // Lê a opção escolhida pelo usuário
+
+        switch (int.Parse(opcao)) // Estrutura de repetição SWITCH para verificar a opção escolhida pelo usuário
+        {
+            case 1:
+                {
+                    _clienteRepositorio.CadastrarCliente();
+                    Menu();
+                    break;
+                }
+            case 2:
+                {
+                    _clienteRepositorio.ExibirClientes();
+                    Menu();
+                    break;
+                }
+            case 3:
+                {
+                    _clienteRepositorio.EditarCliente();
+                    Menu();
+                    break;
+                }
+            case 4:
+                {
+                    _clienteRepositorio.ExcluirCliente();
+                    Menu();
+                    break;
+                }
+            case 5:
+                {
+                    Environment.Exit(0);
+                    break;
+                }
+            default:
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida!");
+                    break;
+                }
+        }
+        }catch(FormatException) 
+        { 
+        Console.WriteLine("Opção inválida, por favor digite uma opção entre 1 e 5!"); 
+        }
+    }
+
+}
